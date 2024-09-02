288a87040fe80afd4a88f66a7f7660f2c54545f7
u_m208h <MNaumenko2@alfabank.ru>
2024-09-02 16:17:35 +0300

    deleted 4 change

--- src/main/java/gossip_utils/service/MessageInitService.java
+++ src/main/java/gossip_utils/service/MessageInitService.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 @Slf4j
 public class MessageInitService {
+
     //third change
     public void initAndGetFilenames(int nodesAmount, int neighboursAmount) {
         List<String> urls = new ArrayList<>();
