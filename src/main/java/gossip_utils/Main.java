97495c55c84cadb2dfde8e6c42f510edf47ff89f
u_m208h <MNaumenko2@alfabank.ru>
2024-09-02 16:00:47 +0300

    aa

--- src/main/java/gossip_utils/Main.java
+++ src/main/java/gossip_utils/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         int nodesAmount = 50;
         int neighboursAmount = 50;
+        //first change
 
         new InitScriptService().init(nodesAmount, neighboursAmount);
 
